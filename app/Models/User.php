2d030d12bc5e36a1b5704fea3280c3d6b0d96926
MelissaMend <melissajmendez23@duck.com>
2025-10-13 19:30:47 -0600

    Se crearon migraciones, seeds y modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,10 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'tipos_id',
+        'username',
         'email',
         'password',
     ];
+    public $timestamps = false;
 
     /**
      * The attributes that should be hidden for serialization.
---
+++ database/migrations/2025_10_13_175534_create_calificaciones_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('calificaciones', function (Blueprint $table) {
+            $table->id();
+            $table->double('calificaciones', 8,2);
+            $table->unsignedBigInteger('materias_x_usuarios_id');
+
+            $table->foreign('materias_x_usuarios_id')->references('id')->on('materias_x_usuarios')->onUpdate('cascade')->onDelete('cascade');
+
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('calificaciones');
+    }
+};
